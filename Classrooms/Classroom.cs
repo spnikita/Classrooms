1188510574397c6e4a0a408c413f1b6d3ba4f9cb
nikita <nikita.yazkov@yandex.ru>
2024-06-18 19:24:37 +0300

    Add project files.

---
+++ Classrooms/Classroom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Classrooms
+{
+    /// <summary>
+    /// Класс
+    /// </summary>
+    internal class Classroom
+    {
+        /// <summary>
+        /// Список учеников
+        /// </summary>
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Classrooms/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classrooms
+{
+    class Program
+    {
+        /// <summary>
+        /// Список классов
+        /// </summary>
+        private static readonly Classroom[] Classes = new[]
+        {
+            new Classroom
+            {
+                Students = { "Evgeniy", "Sergey", "Andrew" },
+            },
+            new Classroom
+            {
+                Students = { "Anna", "Viktor", "Vladimir" },
+            },
+            new Classroom
+            {
+                Students = { "Bulat", "Alex", "Galina" },
+            }
+        };
+
+        static void Main(string[] args)
+        {
+            var allStudents = GetAllStudents(Classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Получить список всех учеников всех классов
+        /// </summary>
+        /// <param name="classes">Классы</param>
+        /// <returns>Список всех учеников всех классов в одном списке</returns>
+        private static ICollection<string> GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(el => el.Students).ToArray();
+        }
+    }
+}
